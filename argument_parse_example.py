b2ef81401667ddfbe2298ce85ddc8df5d137acde
MartinH <hardieck@uni-kassel.de>
2020-11-05 11:59:16 +0100

    bugfix the argument parse example

--- argument_parse_example.py
+++ argument_parse_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3 
 #allows to start direktly from comand line or dubble click
 
-#example and tutorial: https://realpython.com/command-line-interfaces-python-argparse/
+#example and tutorial: https://docs.python.org/3/howto/argparse.html
 #example and tutorial: https://realpython.com/command-line-interfaces-python-argparse/
 
 
@@ -18,30 +18,40 @@ if __name__ == '__main__':
     my_parser = argparse.ArgumentParser(description='List the content of a folder')
 
     # Add the arguments
-    my_parser.add_argument('--Path','-p',
-                       metavar='path',
+    my_parser.add_argument('--path', '-p',
+                       metavar='path_to_list',
                        type=str,
                        help='the path to list')
     
-    my_parser.add_argument('--Path2','-p2',
+    my_parser.add_argument('--Path2', '-p2',
                        metavar='path',
                        type=str,
                        help='the path to list')
-    
 
-    my_parser.add_argument('--verbose','-v',
-                       help='activate  verbose mode')
+    my_parser.add_argument('--verbose', '-v',
+                       metavar='verbose_level',
+                       help='set verbose level')
+    my_parser.add_argument('--loop', '-l',
+                       help='activate loop mode')
 
     # Execute the parse_args() method
     args = my_parser.parse_args()
 
-    input_path = args.Path
 
-    if not os.path.isdir(input_path):
-        print('The path specified does not exist')
-        sys.exit()
+# handle all saved arguments: and do stuff :-)
+    if args.path:
+        input_path = args.path
+        if not os.path.isdir(input_path):
+            print('The path specified does not exist')
+            sys.exit()
+        print('\n'.join(os.listdir(input_path)))
 
-    args = my_parser.parse_args()
+    if args.verbose:
+        print('Verbose level was set to', args.verbose)
+        # could also be a function call
 
+    if args.loop:
+        print('loop mode is on')
+    else:
+        print('loop mode is off')
 
-    print('\n'.join(os.listdir(input_path)))
